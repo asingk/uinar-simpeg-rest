2c71c9b81762a2851124f327140e42b8823d530a
asingk <asingk@proton.me>
2025-08-25 15:53:55 +0700

    fix #7

--- src/main/java/id/ac/arraniry/simpegapi/service/GajiService.java
+++ src/main/java/id/ac/arraniry/simpegapi/service/GajiService.java
@@ -3,7 +3,6 @@ package id.ac.arraniry.simpegapi.service;
 import id.ac.arraniry.simpegapi.entity.Gaji;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface GajiService {
     List<Gaji> saveAll(List<Gaji> gajiList);
